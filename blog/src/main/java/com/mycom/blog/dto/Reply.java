bd4aadc7532ad2a628d2c78dc3f0a2ba8bd56bf5
yjw9303 <yjw9303@naver.com>
2020-12-01 22:20:02 +0900

    댓글 앞단

--- blog/src/main/java/com/mycom/blog/dto/Reply.java
+++ blog/src/main/java/com/mycom/blog/dto/Reply.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
@@ -22,42 +23,39 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-
-
-@SequenceGenerator(
-        name="REPLY_SEQ_GEN", //시퀀스 제너레이터 이름
-        sequenceName="REPLY_SEQ", //시퀀스 이름
-        initialValue=1, //시작값
-        allocationSize=1 //메모리를 통해 할당할 범위 사이즈
-        )
+@SequenceGenerator(name = "REPLY_SEQ_GEN", // 시퀀스 제너레이터 이름
+		sequenceName = "REPLY_SEQ", // 시퀀스 이름
+		initialValue = 1, // 시작값
+		allocationSize = 1 // 메모리를 통해 할당할 범위 사이즈
+)
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@ToString(exclude = { "board","user" })
+@ToString(exclude = { "board", "user" })
 @JsonIdentityInfo(generator = IntSequenceGenerator.class, property = "id")
 public class Reply {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="REPLY_SEQ_GEN")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "REPLY_SEQ_GEN")
 	private int id;
-	
-	@Column(nullable = false, length =  200) 
+
+	@Column(nullable = false, length = 200)
 	private String content;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "board_id")
 	private Board board;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "userno")
 	private User user;
-	
+
 	@CreationTimestamp
 	private Timestamp createDate;
-	
-	
-	
-	
+
+	@ColumnDefault("0")
+	private int secretmode = 0;
+
 }
--- blog/src/main/webapp/WEB-INF/views/post/reply/reply_read.jsp
+++ blog/src/main/webapp/WEB-INF/views/post/reply/reply_read.jsp
@@ -1,10 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <div class="reply container" style="min-height: 200px;">
-	<div class="reply_list">
+	<div class="reply_cutline">
+		댓글 ${board.replyList.size()}
+	</div>
 	
+	<div class="reply_list">
 		<c:forEach var="reply" items="${board.replyList}">
 			<div class="reply">
+				<hr>
 				<input type="hidden" class="reply_id" value="${reply.id}">
 				<div class="img" onclick="location.href='/profile/profileMain/${reply.user.userno}'">
 					<img src="${reply.user.picture}">
@@ -15,6 +19,14 @@
 						<div class="nickname" onclick="location.href='/profile/profileMain/${reply.user.userno}'">
 							${reply.user.nickname}
 						</div>
+						
+						<c:choose>
+							<c:when test = "${board.user.userno == reply.user.userno}">
+							<div class="myReply">
+								&nbsp;작성자&nbsp;
+							</div>
+							</c:when>
+						</c:choose>
 						
 						<div class="btn_ud">
 						<c:choose>
@@ -41,8 +53,14 @@
 	</div>
 	
 	<div class="reply_write">
-		<textarea id="reply_content" rows="3" cols="50" placeholder="댓글을 입력하세요"></textarea>
-		<input type="button" id="btn_reply_write" value="등록">
+		<div class="write_replyContent">
+			<textarea id="reply_content" rows="3" cols="100" placeholder="댓글을 입력하세요"></textarea>
+		</div>
+		
+		<div class="btn_WriteReply">
+			<input type='checkbox' id='secretmode' >비밀댓글
+			<button id="btn_reply_write">등록</button>
+		</div>
 	</div>
 	
 </div>
