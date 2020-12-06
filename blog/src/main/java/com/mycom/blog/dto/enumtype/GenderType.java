c0bd8aed3c802502937f9bb6dfc3ae4e94dc6639
SunBi91 <sp91lsu@gmail.com>
2020-12-06 19:37:56 +0900

    젠더타입 복구

--- blog/src/main/java/com/mycom/blog/dto/enumtype/GenderType.java
+++ blog/src/main/java/com/mycom/blog/dto/enumtype/GenderType.java
@@ -2,5 +2,17 @@ package com.mycom.blog.dto.enumtype;
 
 public enum GenderType {
 
-	MALE, FEMALE;
+	MALE("남"), FEMALE("여");
+
+	String name;
+
+	GenderType(String genderName) {
+		name = genderName;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return name;
+	}
 }
